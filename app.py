43903983f035189f4037aaa5d4bc3d208059e13d
Nate Jones <thebreathlesshashira@gmail.com>
2025-12-02 12:03:48 -0600

    Initial commit with a barebones framework for our app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.template_folder = "templates"
+app.static_folder = "static"
+
+# Define what they user should see when they visit the root URL
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
