6478b009a0a4b1bb206602cacbb958267ac7af2b
Adum05 <amunozguedes@york.edu>
2025-12-04 12:13:14 -0600

    Merge all the changes in main to the branch and Restructure project into package so tests can import app

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.template_folder = "templates"
 app.static_folder = "static"
 
 # Define what they user should see when they visit the root URL
